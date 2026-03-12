6dc2441810521dafc05badd70e96ba43a0ee10f2
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-03-12 18:48:10 +0530

    Time: 98 ms (39.64%), Space: 31.6 MB (65.44%) - LeetHub

--- 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
@@ -1,19 +1,49 @@
 class Solution {
 public:
+
     int minOperations(string s) {
 
         int n = s.size();
         string t = s;
         sort(t.begin(), t.end());
 
-        if(s == t) return 0;
-
-        if(n == 2) return -1;
-
-        if(s[0] == t[0] || s[n-1] == t[n-1]){
-            return 1;
+        if(t == s){
+            return 0;
         }
 
-        return 2;
+        if(n == 2){
+            return -1;
+        }
+
+        char mn = t[0];
+        char mx = t[n-1];
+        bool start = false;
+        bool mid = false, end = false;
+        for(int i = 0; i<n; i++){
+            if(s[i] == mn){
+                if(i== 0){
+                    start = true;
+                }
+                else if(i != n-1){
+                    mid = true;
+                }
+            }
+            if(s[i] == mx){
+                if(i == n-1){
+                    end = true;
+                }
+                else if(i != 0){
+                    mid = true;
+                }
+            }
+        }
+        if(start || end ){
+            return 1;
+        }
+        if(mid ){
+            return 2;
+        }
+        return 3;
+
     }
 };
