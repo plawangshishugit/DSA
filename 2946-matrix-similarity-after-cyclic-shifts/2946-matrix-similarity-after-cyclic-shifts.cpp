22ee50800de99ba3cc294bbe1f651a1d5f737558
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-03-27 23:32:38 +0530

    Time: 0 ms (100%), Space: 31.2 MB (91.3%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int shift = k % n;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                
+                int newCol;
+
+                if(i % 2 == 0){
+                  
+                    newCol = (j + shift) % n;
+                } else {
+                   
+                    newCol = (j - shift % n + n) % n;
+                }
+
+                if(mat[i][j] != mat[i][newCol]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
