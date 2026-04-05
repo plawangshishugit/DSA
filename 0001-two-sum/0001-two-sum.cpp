881363f04fd821bfdec879fd006df7d8cfd7de4d
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-04-05 20:42:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int>ans;
+        unordered_map<int,int>mp;
         for(int i =0; i<n; i++){
             int rem = target - nums[i];
-            for(int j = i+1; j<n; j++){
-                if(nums[j] == rem){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+            if(mp.find(rem) == mp.end()){
+                mp[nums[i]] = i;
+            }
+            else{
+                return {mp[rem], i};
             }
         }
-        return ans;
-        
+        return {};
     }
 };
