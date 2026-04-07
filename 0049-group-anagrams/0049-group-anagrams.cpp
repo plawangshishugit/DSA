13a79e378fe6ecb7b01b4ffae9748df3d4a63106
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-04-07 20:59:48 +0530

    Time: 12 ms (83.24%), Space: 25 MB (66.96%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(string s : strs){
+            string key = s;
+            sort(key.begin(), key.end()); // create key
+            
+            mp[key].push_back(s); // group anagrams
+        }
+
+        vector<vector<string>> ans;
+
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
