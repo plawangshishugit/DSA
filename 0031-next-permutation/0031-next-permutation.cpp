b6a5826e734aa7e6385aa15b990f383b000928c4
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-04-07 05:59:30 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.7%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -12,13 +12,19 @@ public:
             }
         }
 
-
         // if no breakpoint, so reverse entire array
         if(breakPoint == -1){
             reverse(nums.begin(), nums.end());
             return;
         }
-        swap(nums[breakPoint],nums[breakPoint+1]);
+
+        // find element just greater than nums[breakpoint] and swap it wiht breakpoint if you traverse from back then the first element you fount greater than the breakpoint it is the element to swap
+        for(int i = size-1; i> breakPoint; i--){
+            if(nums[i] > nums[breakPoint]) {
+                swap(nums[i], nums[breakPoint]);
+                break;
+            }
+        }
         // reverse the array
         reverse(nums.begin() + breakPoint + 1, nums.end());
     }
