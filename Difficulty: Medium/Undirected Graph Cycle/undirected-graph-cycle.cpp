95fee9073f23712e354c61f0aedfbca4bc851471
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-04-08 06:44:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,43 +1,33 @@
 class Solution {
-  public:
+public:
+    bool dfs(int vertex, int par, vector<vector<int>>& adj, vector<bool>& vis){
+        vis[vertex] = true;
 
-    bool isCycleDFS(vector<vector<int>>& adj, int node, vector<bool>& visited, int parent){
-        
-        visited[node] = true;
+        for(auto child : adj[vertex]){
+            if(child == par) continue;
 
-        for(int neighbour : adj[node]){
+            if(vis[child]) return true;
 
-            if(!visited[neighbour]){
-                if(isCycleDFS(adj, neighbour, visited, node)){
-                    return true;
-                }
-            }
-            else if(neighbour != parent){
-                return true;
-            }
+            if(dfs(child, vertex, adj, vis)) return true;
         }
 
         return false;
     }
 
     bool isCycle(int V, vector<vector<int>>& edges) {
-        
+
         vector<vector<int>> adj(V);
 
-        // convert edge list to adjacency list
-        for(auto &e : edges){
-            int u = e[0];
-            int v = e[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(auto edge : edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
 
-        vector<bool> visited(V,false);
+        vector<bool> vis(V, false);
 
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                if(isCycleDFS(adj,i,visited,-1)){
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                if(dfs(i, -1, adj, vis)){
                     return true;
                 }
             }
