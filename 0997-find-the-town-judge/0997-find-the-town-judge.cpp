bab50737e6c3d8188c4c504694304071b65a46cf
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-04-27 19:25:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int len = trust.size();
-        
-        vector<vector<int>> vis(n+1); // who trusts i
-        vector<int> out(n+1, 0);      // who i trusts
+        vector<int> score(n+1, 0);
 
-        for(int i = 0; i < len; i++){
-            int a = trust[i][0];
-            int b = trust[i][1];
-            vis[b].push_back(a);
-            out[a]++; // a trusts someone
+        for(auto &t : trust){
+            score[t[0]]--;
+            score[t[1]]++;
         }
 
         for(int i = 1; i <= n; i++){
-            if(vis[i].size() == n-1 && out[i] == 0){
-                return i;
-            }
+            if(score[i] == n-1) return i;
         }
 
         return -1;
