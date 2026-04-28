1c14f75816672631c578c5a231b2341f8c80d42a
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-04-28 12:22:50 +0530

    Time: 31 ms (74.49%), Space: 82.1 MB (87.24%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        bool isPossible  = true;
+        vector<int> temp;
+        
         int n = grid.size();
         int m = grid[0].size();
-        int rem = grid[0][0] % x;
-        vector<int>temp;
-        for(int i =0; i<n; i++){
-            for(int j=0; j<m; j++){
-                temp.push_back(grid[i][j]);
-                if(grid[i][j] % x != rem ){
-                isPossible = false;
-            }
+        
+        temp.reserve(n * m);  // avoid reallocations
+        
+        int base = grid[0][0];
+
+        // Flatten + feasibility check
+        for (auto &row : grid) {
+            for (int val : row) {
+                if (abs(val - base) % x != 0) {
+                    return -1;
+                }
+                temp.push_back(val);
             }
         }
-        if(isPossible){
-            sort(temp.begin(),temp.end());
-            int len = temp.size();
-            int mid = temp[(len-1)/2];
-            int cnt = 0; 
-            int sum = 0;
-            for(int i =0; i<len; i++){
-                cnt = abs(temp[i] - mid)/x;
-                sum += cnt;
-            }
-            return sum;
-        }else{
-            return -1;
+
+        // Sort
+        sort(temp.begin(), temp.end());
+
+        // Median
+        int target = temp[temp.size() / 2];
+
+        // Count operations
+        int ops = 0;
+        for (int val : temp) {
+            ops += abs(val - target) / x;
         }
+
+        return ops;
     }
 };
