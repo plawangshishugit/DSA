9d696f82c7a5dcf7f3106639692cfb184d9aadbc
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-05-12 18:47:08 +0530

    Time: 0 ms (100%), Space: 95.3 MB (40.96%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,61 @@
-//Brute
+//Optimized
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        // Always binary search on smaller array
+        if(nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
+
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int i =0; int j = 0;
-        vector<int>temp;
-        while(i<n1 && j<n2){
-            if(nums1[i] < nums2[j]){
-                temp.push_back(nums1[i]);
-                i++;
+
+        int low = 0;
+        int high = n1;
+
+        while(low <= high) {
+
+            int cut1 = (low + high) / 2;
+
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+
+            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+
+            // Correct partition
+            if(l1 <= r2 && l2 <= r1) {
+
+                // Odd length
+                if((n1 + n2) % 2 != 0) {
+
+                    return max(l1, l2);
+                }
+
+                // Even length
+                else {
+
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+                }
             }
-            else{
-                temp.push_back(nums2[j]);
-                j++;
+
+            // Move left
+            else if(l1 > r2) {
+
+                high = cut1 - 1;
+            }
+
+            // Move right
+            else {
+
+                low = cut1 + 1;
             }
         }
-        if(i<n1){
-            for(int t =i; t<n1; t++){
-                temp.push_back(nums1[t]);
-            }
-        }
-        if(j<n2){
-            for(int t =j; t<n2; t++){
-                temp.push_back(nums2[t]);
-            }
-        }
-        int n = temp.size();
-        if(n % 2 != 0){
-            return double(temp[n/2]);
-        }
-        else{
-            return ((double(temp[n/2]+ temp[(n-1)/2]))/double(2));
-        }
+
+        return 0.0;
     }
 };
