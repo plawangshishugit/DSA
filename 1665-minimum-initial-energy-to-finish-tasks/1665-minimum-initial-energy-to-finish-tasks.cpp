22c3674c8976991c0eecb0e86f6428cd859f7b26
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-05-12 15:26:49 +0530

    Time: 35 ms (71.84%), Space: 97.4 MB (63.11%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(),
+        [](vector<int>& a, vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+        int energy = 0;
+        int answer = 0;
+        for(int i = 0; i < tasks.size(); i++) {
+
+            int actual = tasks[i][0];
+            int minimum = tasks[i][1];
+            if(energy < minimum) {
+                answer += (minimum - energy);
+                energy = minimum;
+            }
+            energy -= actual;
+        }
+
+        return answer;
+    }
+};
