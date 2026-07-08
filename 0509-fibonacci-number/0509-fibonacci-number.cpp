5067cfd3d21c630fbdb40b429ee1d8758e3f7a7d
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-07-08 11:38:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (10.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    vector<int>dp;
+    int solve(int n){
+        if(n<=1) return n;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = solve(n-1) + solve(n-2);
+    }
     int fib(int n) {
-        if(n == 0) return 0;
-        if(n ==1) return 1;
-        if(n ==2) return 1;
-        return (fib(n-1) + fib(n-2)); 
+        dp.assign(n+1,-1);
+        return solve(n);
     }
 };
